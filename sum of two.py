56687d9422cd82886a7c5473eeeb05e1e8f3694b
“bditicom” <bditi.com@gmail.com>
2021-05-05 02:47:37 -0700

    first commit

---
+++ sum of two.py
@@ -0,0 +1,25 @@
+#Use to Modeul
+age = int(input("Enter your age :"))
+Result = age + 1
+print("Your age is %d"%age)
+
+print("After one year your age will be ",Result)
+
+
+
+#sum of two numbers
+#print("welcome, this is program for sum of two numbeers ")
+#x=input("enter 1st number ")
+#x=int(x)
+#y=input("enter 2nd numbers ")
+#y=int(y)
+#print(("sum of two number is ")+ str (x+y))
+
+#x=int(input("Enter 1st Number"))
+#y=int(input ("Enter 2nd Number"))
+#print(("sum of two number is")+str(x+y))
+
+#print("welcome, this is program for sum of two numbeers ")
+#x=input("enter 1st number ")
+#y=input("enter 2nd numbers ")
+#print(("sum of two number is ")+(str(int(x)+int(y))))
